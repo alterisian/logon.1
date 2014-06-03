1882318209f704dbcc44744360709946afcd07f8
Prelang Builder <builder@prelang.com>
2014-06-03 10:44:26 +0000

    Generated controller for root route with 'index' action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Logon::Application.routes.draw do
+  get "home/index"
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
